b3654f04b313d24796543c23306bbf43b62e2625
DeviVaishnavi5 <119840593+DeviVaishnavi5@users.noreply.github.com>
2023-05-17 23:09:52 +0530

    #include<stdio.h> int sort(int *v,int n) {     int k,r[2004],j,t;     for(k=0;k<n;k++)     {         r[k]=v[k];     }     for(j=0;j<n-1;j++)     {         for(k=0;k<n-1;k++)         {             if(v[k]>v[k+1])         	{             	t=v[k+1];             	v[k+1]=v[k];             	v[k]=t;         	}         }     }     t=0;     for(k=0;k<n;k++)     {         if(v[k]!=r[k])         {             t=1;         }     }     if(t==1)     {         t=v[n-1]-v[0];     }     return t; } int main() {     int n,k,j,r,a,s=0;     scanf("%d",&n);     for(j=0;j<n;j++)     {         int v[2004];         scanf("%d",&a);         for(k=0;k<a;k++)         {             scanf("%d",&v[k]);         }         printf("%d ",sort(v,a));     }
    }

--- Optimal_Sorting.c
+++ Optimal_Sorting.c
@@ -1,45 +1,51 @@
 #include<stdio.h>
-#include<limits.h>
-int main()
+int sort(int *v,int n)
 {
-    int t,c=0;
-    scanf("%d",&t);
-    while(t--)
+    int k,r[2004],j,t;
+    for(k=0;k<n;k++)
     {
-        int n;
-        scanf("%d",&n);
-        int arr[n];
-        for(int i=0;i<n;i++)
+        r[k]=v[k];
+    }
+    for(j=0;j<n-1;j++)
+    {
+        for(k=0;k<n-1;k++)
         {
-            scanf("%d",&arr[i]);
-        }
-        for(int k=1;k<n;k++)
-        {
-            if(arr[k-1]>arr[k])
-            {
-                c++;
-            }
-        }
-        if(c==0)
-        {
-            printf("%d",c);
-        }
-        else
-        {
-            int min=INT_MAX,max=INT_MIN;
-            for(int l=0;l<n;l++)
-            {
-                if(min>arr[l])
-                {
-                    min=arr[l];
-                }
-                if(max<arr[l])
-                {
-                    max=arr[l];
-                }
-            }
-            printf("%d
-",max-min);
+            if(v[k]>v[k+1])
+        	{
+            	t=v[k+1];
+            	v[k+1]=v[k];
+            	v[k]=t;
+        	}
         }
     }
+    t=0;
+    for(k=0;k<n;k++)
+    {
+        if(v[k]!=r[k])
+        {
+            t=1;
+        }
+    }
+    if(t==1)
+    {
+        t=v[n-1]-v[0];
+    }
+    return t;
+}
+int main()
+{
+    int n,k,j,r,a,s=0;
+    scanf("%d",&n);
+    for(j=0;j<n;j++)
+    {
+        int v[2004];
+        scanf("%d",&a);
+        for(k=0;k<a;k++)
+        {
+            scanf("%d",&v[k]);
+        }
+        printf("%d
+",sort(v,a));
+    }
+
 }
