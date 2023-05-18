38e996006e9fa93b134ab37047320e335ca1aea5
DeviVaishnavi5 <119840593+DeviVaishnavi5@users.noreply.github.com>
2023-05-18 23:31:04 +0530

    #include<stdio.h>
    int main()
    
    {
    
        int n;
    
        scanf("%d",&n);
    
        int arr[n],i,max=0,s,c;
    
        for(i=0;i<n;i++)
    
        {
    
            scanf("%d",&arr[i]);
    
        }
    
        for(i=0;i<n;i++)
    
        {
    
            if(arr[i]>max)
    
            {
    
                max=arr[i];
    
            }
    
        }
    
         int t;
    
         scanf("%d",&t);
    
         for(i=0;i<n;i++)
    
         {
    
         	if((arr[i]+t)>=max)
    
         	{
    
         		printf("True ");
    
    		}
    
    		else
    
    		{
    
    			printf("False ");
    
    		}
    
    	 }
    
    }

--- Greatest_Number_of_Candies.c
+++ Greatest_Number_of_Candies.c
@@ -1,32 +1,61 @@
 #include<stdio.h>
+
 int main()
+
 {
-    int n,e,max;
+
+    int n;
+
     scanf("%d",&n);
-    int i,arr[n];
+
+    int arr[n],i,max=0,s,c;
+
     for(i=0;i<n;i++)
+
     {
+
         scanf("%d",&arr[i]);
+
     }
-    scanf("%d",&e);
-    max=arr[0];
-    for(i=1;i<n;i++)
-    {
-        if(max<arr[i])
-        {
-            max=arr[i];
-        }
-    }
+
     for(i=0;i<n;i++)
+
     {
-        if(arr[i]+e>=max)
+
+        if(arr[i]>max)
+
         {
-            printf("True ");
+
+            max=arr[i];
+
         }
-        else
-        {
-            printf("False ");
-        }
+
     }
-    
+
+     int t;
+
+     scanf("%d",&t);
+
+     for(i=0;i<n;i++)
+
+     {
+
+     	if((arr[i]+t)>=max)
+
+     	{
+
+     		printf("True ");
+
+		}
+
+		else
+
+		{
+
+			printf("False ");
+
+		}
+
+	 }
+
 }
